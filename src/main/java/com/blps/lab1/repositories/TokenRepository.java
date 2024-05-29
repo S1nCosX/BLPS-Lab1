d0b9ed87ad8f15831ea5eb88f295ab8a7121c68b
S1nCosX <90513410+S1nCosX@users.noreply.github.com>
2024-05-29 13:51:28 +0300

    quickfix

--- src/main/java/com/blps/lab1/repositories/TokenRepository.java
+++ src/main/java/com/blps/lab1/repositories/TokenRepository.java
@@ -1,11 +1,17 @@
 package com.blps.lab1.repositories;
 
 import com.blps.lab1.model.token.Token;
+import jdk.jfr.Registered;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.NoRepositoryBean;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface TokenRepository extends JpaRepository<Token, Integer> {
 
     @Query(value = """
@@ -13,6 +19,7 @@ public interface TokenRepository extends JpaRepository<Token, Integer> {
       on t.user.id = u.id\s
       where u.id = :id and (t.expired = false or t.revoked = false)\s
       """)
+    List<Token> findAllValidTokenByUser(Long id);
 
     Optional<Token> findByToken(String token);
 }
